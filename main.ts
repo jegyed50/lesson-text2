7fc800a7561248e0057397e34002a833ce9b3418
picur1 <133989406+picur1@users.noreply.github.com>
2023-06-15 11:36:12 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,16 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(text1.length)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString(text2.charAt(6))
-    basic.showNumber(text2.length)
+    for (let index = 0; index <= text2.length - 1; index++) {
+        basic.showString(text2.charAt(index))
+        basic.pause(500)
+        basic.clearScreen()
+        basic.pause(200)
+    }
+    basic.showString("X")
 })
 let text2 = ""
 let text1 = ""
 text1 = "PUPPY"
 text2 = "CRYSTAL"
 let text3 = "DOGGY"
-basic.forever(function () {
-	
-})
