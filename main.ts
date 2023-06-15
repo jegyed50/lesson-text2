04619be94186f4f4ff1c7ae619b8b0f5814ff373
picur1 <133989406+picur1@users.noreply.github.com>
2023-06-15 11:12:41 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(text1.length)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString(text2.charAt(6))
+    basic.showNumber(text2.length)
+})
+let text2 = ""
+let text1 = ""
+text1 = "PUPPY"
+text2 = "CRYSTAL"
+let text3 = "DOGGY"
+basic.forever(function () {
+	
+})
